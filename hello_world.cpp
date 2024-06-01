1d5637e91a93115a8fbbfb169e0d8b88cdbe945b
Badal Singh Kushwaha <63418727+badal1509@users.noreply.github.com>
2024-06-01 21:49:19 +0530

    __

--- hello_world.cpp
+++ hello_world.cpp
@@ -4,5 +4,12 @@ int main()
 {
     cout<<"hello world" <<endl;
     cout<<"this is mr first program in cpp"<<endl;
-
+    for(int i=1;i<11;i++)
+    {
+        for(int j=1;j<=i;j++)
+        {
+            cout<<"*";
+        }
+        cout<<"\n";
+    }
 }
